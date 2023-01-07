37ccd2f7f484d0bcec829fa4c22003ede0d46b32
Yegor Kozlov <e.kozlov@softteco.com>
2023-01-07 21:46:41 +0300

    Add basic setup for react-i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
-import { Card } from './Card';
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
-export const App = () => (
-  <div className="App">
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', alignItems: 'center' }}>
-      <Card user={{ fullName: 'Yegor Kozlov', role: 'Husband' }} />
-      <Card user={{ fullName: 'Margarita Kozlova', role: 'Wife' }} />
-      <Card user={{ fullName: 'Kiso Kozlov-Yakubousky', role: 'Pet' }} />
-    </div>
-  </div>
-);
+export const App = () => {
+  const { t } = useTranslation();
+
+  return <div className="App">{t('welcomeMessage')}</div>;
+};
---
+++ src/i18n/init.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import commonEN from './resourses/en/common.json';
+
+const resources = {
+  en: {
+    common: commonEN,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  defaultNS: 'common',
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
