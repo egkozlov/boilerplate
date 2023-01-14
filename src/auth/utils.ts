6f2bb1661ee834ce0f7247fdd5e35bb08039c6fd
Yegor Kozlov <e.kozlov@softteco.com>
2023-01-14 20:04:29 +0300

    Test

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -1,2 +1,2 @@
-
-export const is_Email_Invalid = (value: string) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+// eslint-disable-next-line no-useless-escape
+export const isEmailInvalid = (value: string) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
